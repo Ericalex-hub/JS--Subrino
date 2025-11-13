0bd508a4bf1885cd3cc0d320717d0ae7baae043e
Ericalex-hub <ealdsan503@g.educaand.es>
2025-11-13 19:52:42 +0100

    Proyecto-2

--- clase-ejer2/js/UI.js
+++
@@ -1,9 +0,0 @@
-export const UI = {
-    mainCont: null,
-    container:null,
-
-    init: (config) => {
-        UI.mainCont = document.getElementById(config.mainCont);
-        UI.container = document.getElementById(config.container);
-    }, 
-}
--- clase-ejer2/js/main.js
+++
@@ -1,9 +0,0 @@
-import { UI } from "./UI.js";
-import { TimeHandler } from "./TimeHandler.js";
-
-UI.init({
-    main: 'main-container',
-    container: 'container'
-});
-//Poner 5000 = 5s
-TimeHandler.repetir(UI, 10, 5000);
